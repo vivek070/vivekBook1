10cbf8e913a8aabbac7af380e45ae77f3e8a783b
vsahu <vsahu@DESKTOP-QARRUQA>
2022-09-26 16:14:10 +0530

    Add project files.

---
+++ vivekBook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vivekBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
